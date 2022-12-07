d3cf2b73d19fef87f727a689469a50cbac626601
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-12-07 15:12:21 -0300

    Adicionando tratamento de erros no crud

--- src/Controllers/ProdutoControleer.ts
+++ src/Controllers/ProdutoControleer.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 import { Error } from "mongoose";
-import { produtos, Produto } from "../Models/Produto";
+import { produtos } from "../Models/Produto";
 
 // https://bobbyhadz.com/blog/javascript-error-cannot-set-headers-after-they-are-sent-to-client#:~:text=The%20%22Cannot%20set%20headers%20after,single%20response%20for%20each%20request.
 
@@ -13,13 +13,17 @@ import { produtos, Produto } from "../Models/Produto";
 
 export async function listarProdutos(req: Request, res: Response): Promise<Response>{
   const listaProdutos = await produtos.find()
-  return res.json(listaProdutos)
+  return res.status(200).send(listaProdutos)
 } 
 
 export async function listarProdutoPorId(req: Request, res: Response): Promise<Response> {
   const { id } = req.params
-  const produto = await produtos.findById(id)
-  return res.json(produto)
+  const produto = await produtos.findById(id, (err: Error) => {
+    if(err){
+      return res.status(404).send({ mensage: `${err.message} - Produto não encontrado`})
+    }
+  })
+  return res.send(produto)
 } 
 
 export async function cadastrarProduto(req: Request, res: Response): Promise<Response> {
@@ -28,40 +32,26 @@ export async function cadastrarProduto(req: Request, res: Response): Promise<Res
       return res.status(500).send({mensage: `${err.message} - Erro ao cadastrar o produto`})
     }
   })
-  return res.send('Produto cadastrado com sucesso').status(201).json(produto)
+  return res.status(201).send('Produto cadastrado com sucesso').json(produto)
 } 
 
 export async function deletarProduto(req: Request, res: Response): Promise<Response> {
   const { id } = req.params
-  const produto = await produtos.findByIdAndDelete(id)
-  return res.send('Produto deletado com suscesso').json(produto)
+  const produto = await produtos.findByIdAndDelete(id, (err: Error) => {
+    if (err) {
+      return res.status(404).send({ mensage: `${err.message} - Produto não encontrado para ser deletado` })
+    }
+  })
+  return res.status(200).send('Produto deletado com suscesso').json(produto)
 } 
 
 
 export async function atualizarProduto(req: Request, res: Response): Promise<Response> {
   const { id } = req.params
-  const {
-    nome,
-    descricao,
-    img,
-    preco,
-    precoTotal,
-    desconto,
-    ratings,
-    defaultRatings,
-    quantidadeVendas
-  } = req.body
-  const produto = await produtos.findByIdAndUpdate(id, {
-    nome: nome,
-    descricao: descricao,
-    img: img,
-    preco: preco,
-    precoTotal: precoTotal,
-    desconto: desconto,
-    ratings: ratings,
-    defaultRatings: defaultRatings,
-    quantidadeVendas: quantidadeVendas
+  
+  const produto = await produtos.findByIdAndUpdate(id, {$set: req.body}, (err: Error) => {
+  
   })
-  return res.send('Produto atualizado com suscesso').json(produto)
+  return res.status(200).send('Produto atualizado com suscesso').json(produto)
 } 
 
