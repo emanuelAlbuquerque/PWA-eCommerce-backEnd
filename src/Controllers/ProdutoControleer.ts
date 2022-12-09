693c39a83e3bd105653b8f9c3064c3b66b0c8b2f
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-12-09 00:09:10 -0300

    Adicionando tratamento de erros com tryCatch

--- src/Controllers/ProdutoControleer.ts
+++ src/Controllers/ProdutoControleer.ts
@@ -1,5 +1,4 @@
 import { Request, Response } from "express";
-import { Error } from "mongoose";
 import { produtos } from "../Models/Produto";
 
 // https://bobbyhadz.com/blog/javascript-error-cannot-set-headers-after-they-are-sent-to-client#:~:text=The%20%22Cannot%20set%20headers%20after,single%20response%20for%20each%20request.
@@ -11,47 +10,62 @@ import { produtos } from "../Models/Produto";
 // https://github.com/Automattic/mongoose/issues/4192
 
 
-export async function listarProdutos(req: Request, res: Response): Promise<Response>{
-  const listaProdutos = await produtos.find()
+export async function listarProdutos(req: Request, res: Response){
+  const listaProdutos = await produtos.find().populate('categoria').clone()
   return res.status(200).send(listaProdutos)
 } 
 
-export async function listarProdutoPorId(req: Request, res: Response): Promise<Response> {
-  const { id } = req.params
-  const produto = await produtos.findById(id, (err: Error) => {
-    if(err){
-      return res.status(404).send({ mensage: `${err.message} - Produto não encontrado`})
+export async function listarProdutoPorId(req: Request, res: Response) {
+  try {
+    const { id } = req.params
+    const produto = await produtos.findById(id).clone()
+
+    if (!produto) {
+      return res.status(404).send({ mensage: `Produto não encontrado` })
     }
-  })
-  return res.send(produto)
+
+    return res.status(200).send(produto)
+    
+  } catch (error) {
+    return res.status(404).send({ mensage: `${error} Verifique se o tamanho do ID está correto` })
+  }
 } 
 
-export async function cadastrarProduto(req: Request, res: Response): Promise<Response> {
-  const produto = await produtos.create(req.body, (err: Error) => {
-    if(err){
-      return res.status(500).send({mensage: `${err.message} - Erro ao cadastrar o produto`})
-    }
-  })
-  return res.status(201).send('Produto cadastrado com sucesso').json(produto)
-} 
+export async function cadastrarProduto(req: Request, res: Response) {
+    const produto = await produtos.create(req.body)
+    return res.status(201).send('Produto cadastrado com sucesso').json(produto)
+}
+
 
 export async function deletarProduto(req: Request, res: Response): Promise<Response> {
-  const { id } = req.params
-  const produto = await produtos.findByIdAndDelete(id, (err: Error) => {
-    if (err) {
-      return res.status(404).send({ mensage: `${err.message} - Produto não encontrado para ser deletado` })
+  try {
+    const { id } = req.params
+    const produto = await produtos.findByIdAndDelete(id).clone()
+
+    if(!produto){
+      return res.status(404).send({ mensage: `Produto não encontrado para ser deletado` })
     }
-  })
-  return res.status(200).send('Produto deletado com suscesso').json(produto)
+
+    return res.status(200).send('Produto deletado com suscesso').json(produto)
+
+  } catch (error) {
+    return res.status(404).send({ mensage: `${error} - Verifique o ID` })
+  }
 } 
 
 
 export async function atualizarProduto(req: Request, res: Response): Promise<Response> {
-  const { id } = req.params
-  
-  const produto = await produtos.findByIdAndUpdate(id, {$set: req.body}, (err: Error) => {
-  
-  })
-  return res.status(200).send('Produto atualizado com suscesso').json(produto)
+  try {
+    const { id } = req.params
+    const produto = await produtos.findByIdAndUpdate(id, {$set: req.body}).clone()
+
+    if (!produto) {
+      return res.status(404).send({ mensage: `Produto não encontrado para ser Atualizado` })
+    }
+
+    return res.status(200).send('Produto atualizado com suscesso').json(produto)
+  } catch (error) {
+    return res.status(404).send({ mensage: `${error} - Verifique o ID` })
+  }
 } 
 
