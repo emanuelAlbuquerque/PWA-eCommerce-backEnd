04a7efada97aefcc2a5929f5faf0f9632cf7398f
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-12-10 19:59:49 -0300

    Criando o model de Favoritos

---
+++ src/Models/Favoritos.ts
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+import { Produto } from "./Produto";
+
+
+interface Favoritos {
+  produto: Produto[]
+}
+
+
+const favoritoSchema: Schema = new Schema(
+  {
+    usuario: { type: String, required: true, ref: 'usuarios' },
+    produtos: [
+      {
+        produto: { type: mongoose.Schema.Types.ObjectId, required: true, ref: 'produtos' },
+      }
+    ]
+  }
+)
+
+export const carrinho = mongoose.model<Favoritos>('categories', favoritoSchema)
