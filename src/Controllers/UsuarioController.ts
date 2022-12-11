57dccf67ce7257971b6b326e521e8aa8c68ec7ad
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-12-11 14:25:00 -0300

    Criando o controller de usuários e modificando o model de usuarios

---
+++ src/Controllers/UsuarioController.ts
@@ -0,0 +1,75 @@
+import { Request, Response } from "express";
+import { Error } from "mongoose";
+import { usuarios } from "../Models/Usuario";
+
+export class UsuarioController{
+  static async listarUsuario(req: Request, res: Response){
+    const listaUsuarios = await usuarios.find().populate({
+      path: 'carrinho',  // Popula o campo carrinho           
+      populate: [{ path: 'produtos.produto' }] // E popula também o produto dentro do carrinho.produtos
+    }).clone()
+
+    if (!listaUsuarios){
+      return res.status(404).send({ mensage: `Lista Vazia!! Cadastre algun usuário` })
+    }
+
+    return res.status(200).send(listaUsuarios)
+  }
+
+  static async listarUsuarioPorId(req: Request, res: Response){
+    try{
+      const { id } =  req.params
+      const usuario = await usuarios.findById(id).clone()
+
+      if (!usuario) {
+        return res.status(404).send({ mensage: `Usuario não encontrado` })
+      }
+  
+      return res.status(200).send(usuario)
+
+    }catch(err){
+      return res.status(500).send({ mensage: `${err} ID incorreto` })
+    }
+  }
+  
+  static async cadastarUsuario(req: Request, res: Response){
+    try{
+      const usuario = await usuarios.create(req.body)
+      return res.status(201).send('Usuario cadastrado com sucesso')
+    }catch(err){
+      return res.status(500).send(`${err} Erro ao cadastrar o usuário`)
+    }
+  }
+
+  static async atualizarUsuario(req: Request, res: Response){
+    try {
+      const { id } = req.params
+      const usuario = await usuarios.findByIdAndUpdate(id, { $set: req.body }).clone()
+
+      if (!usuario) {
+        return res.status(404).send({ mensage: `Usuario não encontrado` })
+      }
+
+      return res.status(200).send({ mensage: `Usuario atualizado com sucesso` })
+
+    } catch (err) {
+      return res.status(500).send({ mensage: `${err} ID incorreto` })
+    }
+  }
+
+  static async deletarUsuario(req: Request, res: Response){
+    try {
+      const { id } = req.params
+      const usuario = await usuarios.findByIdAndDelete(id).clone()
+
+      if (!usuario) {
+        return res.status(404).send({ mensage: `Usuario não encontrado` })
+      }
+
+      return res.status(200).send({ mensage: `Usuario deletado com sucesso` })
+
+    } catch (err) {
+      return res.status(500).send({ mensage: `${err} ID incorreto` })
+    }
+  }
+}
--- src/Models/Usuario.ts
+++ src/Models/Usuario.ts
@@ -1,17 +1,23 @@
 import mongoose, { Schema } from "mongoose";
-import { Avaliacao } from "./Avaliacao";
+import { carrinho } from "./Carrinho";
+
 
 
 export type Usuario = {
-
+  _id: string
+  nome: string
+  email: string
 }
 
 
 
 const usuarioSchema: Schema = new Schema(
   {
-    nome: {type: String, require: true},
-  }
+    nome: {type: String, required: true},
+    email: { type: String, required: true, unique: true, lowercase: true },
+
+    carrinho: { type: String, ref: 'carrinho' }
+  }, { versionKey: false }
 )
 
 export const usuarios = mongoose.model<Usuario>('usuarios', usuarioSchema)
