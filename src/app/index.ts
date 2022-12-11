ce9e7223c99550e633733013921fe0a091216f5d
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-12-11 14:21:19 -0300

    Adicionando rotas de carrinho e de usuarios

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,6 +4,8 @@ import { AvaliacaosRouter } from '../Routes/AvaliacoesRouter';
 import { CategoriasRouter } from '../Routes/CategoriasRouter';
 import { CuponsRouter } from '../Routes/CupomRouter';
 import { ProdutosRouter } from '../Routes/ProdutosRouter'
+import { UsuarioRouter } from '../Routes/UsuarioRouter';
+import { CarrinhoRouter } from '../Routes/CarrinhoRouter';
 
 
 export const app = express();
@@ -13,33 +15,8 @@ app.use(ProdutosRouter)
 app.use(CategoriasRouter)
 app.use(CuponsRouter)
 app.use(AvaliacaosRouter)
+app.use(UsuarioRouter)
+app.use(CarrinhoRouter)
 
 
 
-
-// route.post('/novoProduto', (req: Request, res: Response) => {
-//   produtos.push(req.body);
-//   res.status(201).send('Produto cadastrado com sucesso');
-// })
-
-// route.put('/atualizaProduto/:id', (req: Request, res: Response) => {
-//   const idProduto = procuraProduto(req.params.id)
-//   produtos[idProduto].nome = req.body.nome
-
-//   res.send('livro alualizado com sucesso')
-// })
-
-// route.get('/produto/:id', (req: Request, res: Response) => {
-//   const produto = procuraProduto(req.params.id)
-//   res.json(produtos[produto])
-// })
-
-// route.delete('/deletaProduto/:id', (req: Request, res: Response) => {
-//   const { id } = req.params
-//   const novoProduto = produtos.splice(parseInt(id), 1)
-//   res.json(novoProduto)
-// })
-
-// const procuraProduto = (idProduto: string) => {
-//   return produtos.findIndex(produto => produto.id === idProduto)
-// }
