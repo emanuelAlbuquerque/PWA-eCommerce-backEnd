b128dc23b113aefe3b0d6c36e9d7f1f2368b49e0
emanuelAlbuquerque <emanuelalbuquerque16@gmail.com>
2022-12-12 21:32:00 -0300

    Adicioando as outras rotas

--- src/Routes/CarrinhoRouter.ts
+++ src/Routes/CarrinhoRouter.ts
@@ -4,5 +4,8 @@ import { CarrinhoController } from '../Controllers/CarrinhoController'
 export const CarrinhoRouter = Router()
 
 
+CarrinhoRouter.put('/removerProdutos/:email/:id', CarrinhoController.removerProdutoCarrinho)
+CarrinhoRouter.put('/adicionarProdutos/:email', CarrinhoController.adicionarProdutoCarrinho)
+CarrinhoRouter.get('/listarCarrinhoUsuario/:email', CarrinhoController.listarCarrinhoUsuario)
 CarrinhoRouter.get('/listarCarrinhos', CarrinhoController.listarItensCarrinho)
 CarrinhoRouter.post('/cadastrarCarrinho', CarrinhoController.criarCarrinho)
